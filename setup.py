ff4c8c14b8034dde5934eee07dea6958824e7efe
gabriel <gabrielmpp2@gmail.com>
2019-11-04 12:45:35 +0000

    initial commit

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='LagrangianCoherence',
     version='v0.1',
-    packages=[''],
+    packages=['LCS'],
     url='https://github.com/gabrielmpp/LagrangianCoherence/archive/v0.1.tar.gz',
     license='MIT',
     author='gabriel',
